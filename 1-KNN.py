7b56758746cedc84916eb01f985df199a4c0ad14
Şevval Nur GÜMÜŞER <147150993+sevvalgm@users.noreply.github.com>
2024-09-23 23:57:10 +0300

    Create 1-KNN.py
    Bu Jupyter notebook projesi, K-En Yakın Komşu (KNN) algoritması kullanarak hem sınıflandırma hem de regresyon işlemlerini içeren bir makine öğrenmesi uygulamasıdır.

---
+++ 1-KNN.py
@@ -0,0 +1,100 @@
+# sklearn: ML Library
+from sklearn.datasets import load_breast_cancer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#(1) Veri seti incelemesi
+cancer=load_breast_cancer()
+df=pd.DataFrame(data=cancer.data , columns = cancer.feature_names)
+df["target"]=cancer.target
+
+#(2) Makine Öğrenmesi Modelinin Seçilmesi -KNN Sınıflandırması 
+#(3) Modelin Train Edilmesi 
+X= cancer.data #features
+y= cancer.target #target
+
+#train test split
+X_train, X_test, y_train, y_test=train_test_split(X, y, test_size= 0.3, random_state=42 )
+
+#olceklendirme
+scaler=StandardScaler()
+X_train=scaler.fit_transform(X_train)
+X_test=scaler.transform(X_test)
+
+
+#knn modeli olustur ve train et
+knn= KNeighborsClassifier(n_neighbors=3) #Model oluşturma komşu parametresini unutma******
+knn.fit(X_train, y_train) #fit fonksiyonu verimizi (samples + target) kullanarak knn algoritmasını eğitiir.
+
+
+#(4) Sonuçların Değerlendirilmesi :test
+y_pred=knn.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Dogruluk:", accuracy)
+
+
+conf_matrix = confusion_matrix(y_test,y_pred)
+print("confusion_matrix:")
+print(conf_matrix)
+
+#(5) Hiperparametre Ayarlanması [amaç doğruluğu arttırmak]
+
+#KNN: Hyperparametresi = K 
+ #K= 1,2,3 ....  N 
+ #Accuracy: %A, %8, %C ....
+accuracy_values= []
+k_values= []
+for k in range(1,21):
+    knn=KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_pred=knn.predict(X_test)
+    accuracy=accuracy_score(y_test, y_pred)
+    accuracy_values.append(accuracy)
+    k_values.append(k)
+#Grafikte K degiskenleri 
+plt.figure()
+plt.plot(k_values, accuracy_values, marker= "o", linestyle="-")
+plt.title("K değerine gore dogruluk")
+plt.xlabel("K Degeri")
+plt.ylabel("dogruluk")
+plt.xticks(k_values)
+plt.grid(True)
+
+# %% kendimiz oluşturduk veri setini
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsRegressor
+    
+
+X = np.sort(5 * np.random.rand(40, 1), axis=0) #uniform dagilim
+y = np.sin(X).ravel() #target
+
+#plt.scatter(X,y)
+
+# add noise 
+y[::5] += 1 * (0.5 - np.random.rand(8))
+
+#plt.scatter(X,y)
+T = np.linspace(0, 5, 500)[:, np.newaxis]
+
+for i, weight in enumerate(['uniform', 'distance']):
+
+    knn = KNeighborsRegressor(n_neighbors=5, weights = weight)
+    y_pred = knn.fit(X, y).predict(T)
+    
+    plt.subplot(2, 1,i+1)
+    plt.figure()
+    plt.scatter(X, y, color= "green", label = "data")
+    plt.plot(T, y_pred, color = "blue", label = "prediction")
+    plt.axis("tight")
+    plt.legend()
+    plt.title(" KNN Regressor weights = {}".format(weight))
+    
+plt.tight_layout()
+plt.show()
+
